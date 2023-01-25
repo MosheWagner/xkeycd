9de2dac197d999c51b02a85f673a000bdacf39d1
Moshe Wagner <moshe.wagner@gmail.com>
2023-01-25 12:11:03 +0200

    Added multiple keyword support (and some other small site improvements)

--- site/xkeycd.js
+++ site/xkeycd.js
@@ -1,13 +1,13 @@
-var availableTags = [];
-var comic_scores = {};
+const availableTags = [];
+const comic_scores = {};
 
-var comic_titles = {};
-var comic_links = {};
-var comic_tooltips = {};
+const comic_titles = {};
+const comic_links = {};
+const comic_tooltips = {};
 
-var TRANSCRIPT_FACTOR = 4;
-var EXPLANATION_FACTOR = 1;
-var MAX_RES = 10;
+const TRANSCRIPT_FACTOR = 4;
+const EXPLANATION_FACTOR = 1;
+const MAX_RES = 10;
 
 $.getJSON("scores.json", function(json) {
     $.each( json, function( key, val ) {
@@ -26,27 +26,55 @@ $.getJSON("comics_meta.json", function(json) {
     });
 });
 
+function showComics(keywords) {
+	const scores = {};
+	for (const keyword of keywords) {
+		$.each(comic_scores[keyword], function(comic_id, kw_score) {
+			prev_score = scores[comic_id] || 0;
+            scores[comic_id] = prev_score + (TRANSCRIPT_FACTOR * kw_score.transcript_score || 0 + EXPLANATION_FACTOR * kw_score.explanation_score || 0);
+        });
+	}
+	
+	 scoresSorted = Object.keys(scores).sort(function(a,b){return scores[a]-scores[b]}).reverse().slice(0, MAX_RES);
+	// console.log(scoresSorted);   
+        
+	$('#images').empty();
+	scoresSorted.forEach(function(comic_id) {
+		const str_id = String(comic_id)
+		 $('#images').append('<li>' + '<a href="https://xkcd.com/' + str_id + '">' +  'XKCD ' + str_id + ': ' + comic_titles[str_id] + '</a> <br><img src=' + comic_links[str_id] + '><br><small>' + comic_tooltips[str_id] + '</small><br><br><br><br>');
+	} );
+}
+
+function addKeyword(keyword) {
+	$('#tags-input').tagsinput('add', keyword);
+}
+
 $( function() {
-    $( "#kws" ).autocomplete({
+	$('#tags-input').tagsinput();
+	
+    $("#kws" ).autocomplete({
       minLength: 3,
       delay: 200,
       source: availableTags,
-      select: function( event, ui ) {
-        scores = {};
-          
-        var keyword = ui.item.value;
-        $.each(comic_scores[keyword], function( comic_id, kw_score ) {
-            scores[comic_id] = TRANSCRIPT_FACTOR * kw_score.transcript_score || 0 + EXPLANATION_FACTOR * kw_score.explanation_score || 0;
-        });
-        
-        scoresSorted = Object.keys(scores).sort(function(a,b){return scores[a]-scores[b]}).reverse().slice(0, MAX_RES);
-        // console.log(scoresSorted);   
-        
-        $('#images').empty();
-        scoresSorted.forEach(function(comic_id) {
-            var str_id = String(comic_id)
-             $('#images').append('<li>' + '<a href="https://xkcd.com/' + str_id + '">' +  'XKCD ' + str_id + ': ' + comic_titles[str_id] + '</a> <br><img src=' + comic_links[str_id] + '><br><small>' + comic_tooltips[str_id] + '</small><br><br><br><br>');
-        } );
-      }
+      select: function( event, ui) {
+		  addKeyword($(this).val());
+		  $(this).val(''); return false;
+	  }
     });
+	
+	$('#kws').keypress(function(event) {
+        if (event.keyCode == 13) {
+		  addKeyword($(this).val());
+		  $(this).val('');
+		  $(this).blur(); 
+        }
+    });
+	
+	$('#tags-input').on('itemAdded', function(event) {
+	  showComics($('#tags-input').tagsinput('items'));
+	});
+	
+	$('#tags-input').on('itemRemoved', function(event) {
+	  showComics($('#tags-input').tagsinput('items'));
+	});
 } );
